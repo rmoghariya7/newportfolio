7a0191e8cb607a1dc38e75822a9c95db7c19ede3
rmoghariya7 <rmoghariya7@gmail.com>
2023-02-26 12:39:12 +0530

    add js scroll

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const section2 = document.querySelector(".section-2");
 
 const skills = document.querySelector('.nav-link-skills')
 skills.addEventListener('click',function(e){
-    const scrollPosition = section2.getBoundingClientRect();
-    window.scrollTo(
-        scrollPosition.left + window.pageXOffset,
-        scrollPosition.top + window.pageYOffset
-    )
+    section2.scrollIntoView({behavior:"smooth"})
 });
 
 
@@ -14,11 +10,7 @@ const projects = document.querySelector(".projects");
 
 const projectsScroll = document.querySelector('.nav-link-projects')
 projectsScroll.addEventListener('click',function(e){
-    const scrollPosition = projects.getBoundingClientRect();
-    window.scrollTo(
-        scrollPosition.left + window.pageXOffset,
-        scrollPosition.top + window.pageYOffset
-    )
+   projects.scrollIntoView({behavior:"smooth"})
 });
 
 
@@ -28,9 +20,5 @@ const contacts = document.querySelector(".contact");
 
 const contactScroll = document.querySelector('.nav-link-contacts')
 contactScroll.addEventListener('click',function(e){
-    const scrollPosition = contacts.getBoundingClientRect();
-    window.scrollTo(
-        scrollPosition.left + window.pageXOffset,
-        scrollPosition.top + window.pageYOffset
-    )
+   contacts.scrollIntoView({behavior:"auto"})
 });
